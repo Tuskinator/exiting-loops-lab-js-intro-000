5043ac9292d245cdda9756cbfe3fa591b3ffa5dc
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-07-08 14:01:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function findBy(array, findFn) {
   var length = array.length
   var found = 0
   while (i < length) {
-    if (array[i] == findFn) {
+    if (i == findFn) {
       found = i
       i = i + 1
     } else {
